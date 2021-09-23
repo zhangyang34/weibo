f6a5c07d095ef8c981602ecde7b25bcfd95da329
zhangyang34 <zhangyang@wufeng-network.com>
2021-09-23 18:07:24 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,4 +15,14 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+    // 注册表单
+    public function  store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|unique:users|max50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        return;
+    }
+
 }
