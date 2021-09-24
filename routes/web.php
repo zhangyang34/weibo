f6fc8e5b3ab0540bc9d9b0b6ae1da2d5c64fbd17
zhangyang34 <zhangyang@wufeng-network.com>
2021-09-24 16:12:30 +0800

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,12 @@ Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 
 //激活路由
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//填写 Email 的表单
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+//处理表单提交，成功的话就发送邮件，附带 Token 的链接
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+//显示更新密码的表单，包含 token
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+//对提交过来的 token 和 email 数据进行配对，正确的话更新密码
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
